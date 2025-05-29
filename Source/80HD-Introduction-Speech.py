7f71dabcbae9da151f3fab0d82f447e81000790f
eberkenpas <eric@secondstarrobotics.com>
2025-05-29 19:57:39 -0400

    stray commit

--- Source/80HD-Introduction-Speech.py
+++ Source/80HD-Introduction-Speech.py
@@ -6,6 +6,7 @@ voices = engine.getProperty('voices')
 #Set voice speed
 rate = engine.getProperty('rate')
 engine.setProperty('rate', rate-50)
+engine.setProperty('voice', voices[2].id)  # Try different indexes
 
 #engine.say("Hello my name is 80 H D")
 #engine.runAndWait()
